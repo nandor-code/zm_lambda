bcd998b5c50c3d798254017fbb9d25c77d61b4c7
Zoltaroth <nszots@daybreakgames.com>
2020-02-29 09:22:15 -0800

    Added immage annotation support for faces and objects.

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,11 @@ import urllib3
 import hashlib
 import cv2
 
+colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 
+          'yellow': (0, 255, 255),'magenta': (255, 0, 255), 
+          'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0), 
+          'gray': (125, 125, 125), 'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
+
 http = urllib3.PoolManager()
 
 print('Loading function')
@@ -182,10 +187,11 @@ def post_image(channel, msg, img):
     print( r.data )
 
 def annotate_img(img_data, person, faces, objects):
-    
     if( len(faces) == 0 ):
         return img_data
     
+    # More than anything I wish I could do this in memory but cv2 apparently sucks 
+    # and has a really hard time reading from memory so here we are...
     tmp_filename='/tmp/my_image.jpg'
     f = open(tmp_filename, "wb")
     f.write(img_data)
@@ -193,24 +199,51 @@ def annotate_img(img_data, person, faces, objects):
     
     src_image  = cv2.imread(tmp_filename)
     
-    height = src_image.shape[0]
-    width  = src_image.shape[1]
-    
-    left_face = faces["SearchedFaceBoundingBox"]["Left"] * width
-    top_face  = faces["SearchedFaceBoundingBox"]["Top"]  * height
+    draw_face_data( src_image, person, faces )
 
-    right_face  = left_face + ( faces["SearchedFaceBoundingBox"]["Width"]  * width )
-    bottom_face = top_face  + ( faces["SearchedFaceBoundingBox"]["Height"] * height )
-    #print(faces)
-    print(objects)
-    
-    # Highlight their mug
-    cv2.rectangle(src_image, (int(left_face), int(top_face)), (int(right_face), int(bottom_face)), (255, 0, 255), 2)
-    
-    # Label it.
-    cv2.putText(src_image, person, (int(left_face), int(top_face-20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+    draw_object_data( src_image, objects )
 
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     result, img_data = cv2.imencode('.jpg', src_image, encode_param)
     
     return img_data
+    
+def draw_object_data( img, objects ):
+    height = img.shape[0]
+    width  = img.shape[1]
+    
+    labels = objects['Labels']
+    for label in labels:
+        if label["Confidence"] > 95:
+            for inst in label["Instances"]:
+                if inst["Confidence"] > 95:
+                    box = inst["BoundingBox"]
+                    if box:
+                        left, top, right, bott = get_bounding_box( box, width, height )
+                        draw_box_with_text( img, left, top, right, bott, label["Name"], colors["magenta"], colors["magenta"] )
+                
+
+def draw_face_data(img, person, faces):
+    height = img.shape[0]
+    width  = img.shape[1]
+    
+    box = faces["SearchedFaceBoundingBox"]
+    
+    left, top, right, bott = get_bounding_box( box, width, height )
+
+    draw_box_with_text( img, left, top, right, bott, person, colors["green"], colors["green"] )
+
+def draw_box_with_text(img, xa, xb, ya, yb, text, box_color, text_color ):
+    # Create the bounding box.
+    cv2.rectangle(img, (int(xa),int(xb)), (int(ya),int(yb)), box_color, 2)
+    
+    # Label it.
+    cv2.putText(img, text, (int(xa), int(xb-20)), cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
+
+def get_bounding_box( box, width, height ):
+    left  = box["Left"] * width
+    top   = box["Top"]  * height
+    right = left + ( box["Width"]  * width )
+    bott  = top  + ( box["Height"] * height )
+    
+    return left, top, right, bott
